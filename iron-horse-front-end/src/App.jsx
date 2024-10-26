4f641dd783f5b358e5fc416766055b8f7efa7f9c
cristinasstemmer <cristinasstemmer@gmail.com>
2024-10-26 18:10:53 -0300

    feat: tela de cadastre seu veiculo

--- iron-horse-front-end/src/App.jsx
+++ iron-horse-front-end/src/App.jsx
@@ -1,5 +1,14 @@
-function App() {
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import CarRegister from './CarRegister/CarRegister';
 
-}
+const App = () => {
+  return (
+    <div>
+    
+      <CarRegister />
+    </div>
+  );
+};
 
-export default App
+export default App;
