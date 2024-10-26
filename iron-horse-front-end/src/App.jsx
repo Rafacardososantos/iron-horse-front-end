6d1bd185b1eee1fbd541dde7952c25f3aebab83b
cristinasstemmer <cristinasstemmer@gmail.com>
2024-10-26 16:54:40 -0300

    feat: tela cadastro de veiculos (mais informacoes)

--- iron-horse-front-end/src/App.jsx
+++ iron-horse-front-end/src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
+import MoreInformation from './CarRegister-MoreInformation/MoreInformation'
 
+const App = () => {
+  return (
+    <div>
+    
+      <MoreInformation />
+    </div>
+  )
 }
 
 export default App
