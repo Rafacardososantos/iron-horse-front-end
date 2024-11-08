1fc8e7ed3e147e84e4682eb894b1477920ff7b2f
cristinasstemmer <cristinasstemmer@gmail.com>
2024-11-07 22:44:37 -0300

    refactor: reorganizacao das telas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import ImageRegister from './ImageRegister/ImageRegister'
-
-=======
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import CarRegister from './CarRegister/CarRegister';
 
-import MoreInformation from './CarRegister-MoreInformation/MoreInformation'
-
->>>>>>> f7e89cc274df038d96d27563f9136b71e5140e8f
 const App = () => {
   return (
     <div>
-    
-<<<<<<< HEAD
       <ImageRegister />
-=======
-      <MoreInformation />
->>>>>>> f7e89cc274df038d96d27563f9136b71e5140e8f
     </div>
   )
 }
--- src/ImageRegister/ImageRegister.jsx
+++ src/ImageRegister/ImageRegister.jsx
@@ -8,23 +8,6 @@ const VehicleRegistrationModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const fileInputRef = useRef(null);
   const [vehicleData, setVehicleData] = useState({
-    carChange: "",
-    trunkCapacity: "",
-    headlightBulb: "",
-    "checkbox-grid": {
-      checkboxA: false,
-      checkboxB: false,
-      checkboxC: false,
-      checkboxD: false,
-      checkboxE: false,
-      checkboxF: false,
-      checkboxG: false,
-      checkboxH: false,
-      checkboxI: false,
-      checkboxJ: false,
-      checkboxK: false,
-      checkboxL: false,
-    },
     consentCheckboxes: {
       checkbox1: false,
       checkbox2: false,
@@ -93,94 +76,7 @@ const VehicleRegistrationModal = () => {
         />
 
         <img src="../img/carro-ilustracao-de-transporte.png" alt="Logo" className="modal-image" />
-        <form onSubmit={handleSubmit} className="three-column-form">
-          <div className="form-group">
-            <label>Câmbio</label>
-            <select name="carChange" value={vehicleData.carChange} onChange={handleInputChange} required>
-              <option value=""></option>
-              <option value="manual">Manual</option>
-              <option value="automatico">Automático</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Porta-malas</label>
-            <input 
-              type="text" 
-              name="trunkCapacity" 
-              value={vehicleData.trunkCapacity} 
-              onChange={handleInputChange} 
-              placeholder="capacidade em litros" 
-              required 
-            />
-          </div>
-          <div className="form-group">
-            <label>Lâmpada do Farol</label>
-            <select name="headlightBulb" value={vehicleData.headlightBulb} onChange={handleInputChange} required>
-              <option value=""></option>
-              <option value="halogena">Halógena</option>
-              <option value="led">LED</option>
-              <option value="xenon">Xenon</option>
-              <option value="super-branca">Super Branca</option>
-            </select>
-          </div>
-        </form>
-
-        <label>Itens de Conforto e Adicionais</label>
-        <div className="checkbox-grid">
-          <label>
-            <input type="checkbox" name="checkboxA" checked={vehicleData.consentCheckboxes.checkboxA} onChange={handleInputChange} />
-            Isulfim
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxB" checked={vehicleData.consentCheckboxes.checkboxB} onChange={handleInputChange} />
-            Tag - Pedágio
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxC" checked={vehicleData.consentCheckboxes.checkboxC} onChange={handleInputChange} />
-            Segredo Anti Furto
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxD" checked={vehicleData.consentCheckboxes.checkboxD} onChange={handleInputChange} />
-            Multimídia
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxE" checked={vehicleData.consentCheckboxes.checkboxE} onChange={handleInputChange} />
-            Ar condicionado
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxF" checked={vehicleData.consentCheckboxes.checkboxF} onChange={handleInputChange} />
-            Vidros e Travas Elétricas
-          </label>
-        </div>
-
-        <label>Itens de Segurança</label>
-        <div className="checkbox-grid">
-          <label>
-            <input type="checkbox" name="checkboxG" checked={vehicleData.consentCheckboxes.checkboxG} onChange={handleInputChange} />
-            Triângulo
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxH" checked={vehicleData.consentCheckboxes.checkboxH} onChange={handleInputChange} />
-            Macaco
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxI" checked={vehicleData.consentCheckboxes.checkboxI} onChange={handleInputChange} />
-            Chave de Roda
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxJ" checked={vehicleData.consentCheckboxes.checkboxJ} onChange={handleInputChange} />
-            Estepe
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxK" checked={vehicleData.consentCheckboxes.checkboxK} onChange={handleInputChange} />
-            Extintor de Incêndio
-          </label>
-          <label>
-            <input type="checkbox" name="checkboxL" checked={vehicleData.consentCheckboxes.checkboxL} onChange={handleInputChange} />
-            Alarme
-          </label>
-        </div>
-
+      
         <label>Imagens do Veículo</label>
         <span>Imagem de Capa ou Destaque do Anúncio</span>
         <div className="image-container" onClick={handleImageClick}>
@@ -231,6 +127,8 @@ const VehicleRegistrationModal = () => {
             Atesto para os devidos fins legais que todas às informações e/ou conteúdo de imagem fornecidas são verdadeiras, se valendo e fazendo cumprir as normativas da Lei Geral de Proteção de Dados (LGPD - Lei nº 13.709/2018), o Código Civil Brasileiro (Lei nº 10.406/2002) e o previsto pelo Artigo 299 do Código Penal Brasileiro, que tipifica como crime a falsidade ideológica.
           </label>
         </div>
+
+        <button id="cancel-button" type="submit">Cancelar</button>
         <button id="register-button" type="submit">Prosseguir</button>
       </Modal>
     </div>
