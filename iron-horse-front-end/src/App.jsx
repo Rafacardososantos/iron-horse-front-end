b85135d8229a990737981c69cc96a7a4c4011899
cristinasstemmer <cristinasstemmer@gmail.com>
2024-11-11 20:56:03 -0300

    refactor: preparacao pro merge

--- iron-horse-front-end/src/App.jsx
+++ iron-horse-front-end/src/App.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-import MoreInformation from './CarRegister-MoreInformation/MoreInformation'
+import CarRegister from './CarRegister/CarRegister';
 
 const App = () => {
   return (
     <div>
     
-      <MoreInformation />
+      <CarRegister />
     </div>
   )
 }
