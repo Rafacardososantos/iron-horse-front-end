e34cbb812feb7be101ae46be5673ea244516c23f
Emmanoel <emmanoel_95@hotmail.com>
2024-11-12 17:31:08 -0300

    refactor(CreateAccount): add to Modal and remove all forms

--- iron-horse-front-end/src/Create-Account/CreateAccount.jsx
+++ iron-horse-front-end/src/Create-Account/CreateAccount.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./CreateAccount.css";
 import showPasswordIcon from "../img/Visualização_Permitida.png";
 import hidePasswordIcon from "/img/Visualizar.png";
-import closeIcon from "/img/close-Icon.png"; 
+import Modal from '../components/Modal/Modal';
 
 
 const CreateAccount = ({ onClose }) => { 
@@ -32,6 +30,12 @@ const CreateAccount = ({ onClose }) => {
       checkbox5: false,
     },
   }); 
+  const [isModalOpen, setModalOpen] = useState(true);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
@@ -108,240 +112,232 @@ const CreateAccount = ({ onClose }) => {
   };
 
   return (
-    <div className="teste">
-      <div className="create-account-content" onClick={handleOutsideClick}>
-        <div className="react-modal-content">
-        <button className="close-button" onClick={onClose}>
-            <img src={closeIcon} alt="Fechar" />
-          </button>
-          <h2>Crie a sua Conta</h2>
-          <div className="postImage">
-            <div onClick={handleImageClick} style={{ cursor: "pointer" }}>
-              <img
-                src={
-                  formData.image
-                    ? URL.createObjectURL(formData.image)
-                    : "/img/Perfil-Usuario.png"
-                }
-                alt="Logo"
-                className="modal-image"
-              />
-              <span>Anexar Imagem</span>
-            </div>
-            <input
-              type="file"
-              id="fileInput"
-              name="image"
-              accept="image/*"
-              style={{ display: "none" }}
-              onChange={handleChange}
-            />
-          </div>
+    <Modal isOpen={isModalOpen} onClose={onClose}>
+    <h2>Crie a sua Conta</h2>
+    
+    <form onSubmit={handleSubmit} className="registration-form">
+      <div className="postImage">
+        <div onClick={handleImageClick} style={{ cursor: "pointer" }}>
+          <img
+            src={
+              formData.image
+                ? URL.createObjectURL(formData.image)
+                : "/img/Perfil-Usuario.png"
+            }
+            alt="Logo"
+            className="modal-image"
+          />
+          <span>Anexar Imagem</span>
+        </div>
+        <input
+          type="file"
+          id="fileInput"
+          name="image"
+          accept="image/*"
+          style={{ display: "none" }}
+          onChange={handleChange}
+        />
+      </div>
 
-          <div className="form-group full-width">
-            <label>Nome Completo</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <form onSubmit={handleSubmit} className="two-column-form">
-            <div className="form-group">
-              <label>Data de Nascimento</label>
-              <input
-                type="date"
-                name="birthday"
-                value={formData.birthday}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>CNH</label>
-              <input
-                type="text"
-                name="number_driver_license"
-                value={formData.number_driver_license}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </form>
-          <div className="form-group">
-            <label>CEP</label>
-            <input
-              name="zipCode"
-              value={formData.zipCode}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <form onSubmit={handleSubmit} className="two-column-form">
-            <div className="form-group">
-              <label>Rua</label>
-              <input
-                type="text"
-                name="street_name"
-                value={formData.street_name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Número</label>
-              <input
-                type="text"
-                name="number"
-                value={formData.number}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </form>
-          <form onSubmit={handleSubmit} className="two-column-form">
-            <div className="form-group">
-              <label>Bairro</label>
-              <input
-                type="text"
-                name="district"
-                value={formData.district}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Cidade</label>
-              <input
-                type="text"
-                name="city"
-                value={formData.city}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </form>
-          <form onSubmit={handleSubmit} className="two-column-form">
-            <div className="form-group">
-              <label>Celular</label>
-              <input
-                type="tel"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <span>Será enviado um e-mail de confirmação</span>
-          </form>
-          <form onSubmit={handleSubmit} className="two-column-form">
-            <div className="form-group">
-            <label>Senha</label>
-            <input
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="password-register"
-            />
-            <img
-              src={showPassword ? hidePasswordIcon : showPasswordIcon}
-              alt="Ícone de visualização"
-              className="password-icon"
-              onClick={togglePasswordVisibility}
-              />
-            </div>
-            <div className="form-group">
-              <label>Confirmar Senha</label>
-              <input
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="password-register"
-              />
-              <img
-                src={showPassword ? hidePasswordIcon : showPasswordIcon}
-                alt="Ícone de visualização"
-                className="password-icon"
-                onClick={togglePasswordVisibility}
-                />
-            </div>
-          </form>
-          <span>
-            A senha deve conter pelo menos 12 caracteres sendo obrigatório ter
-            uma letra maiúscula, um número e um símbolo alfanumérico.
-          </span>
+      <div className="form-group full-width">
+        <label>Nome Completo</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-          <div className="checkbox-group">
-            <label>
-              <input
-                type="checkbox"
-                name="checkbox1"
-                checked={formData.checkboxes.checkbox1}
-                onChange={handleChange}
-              />
-              Aceita receber comunicações via WhatsApp
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                name="checkbox2"
-                checked={formData.checkboxes.checkbox2}
-                onChange={handleChange}
-              />
-              Aceita os Termos de Uso
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                name="checkbox3"
-                checked={formData.checkboxes.checkbox3}
-                onChange={handleChange}
-              />
-              Aceita a Política de Privacidade
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                name="checkbox4"
-                checked={formData.checkboxes.checkbox4}
-                onChange={handleChange}
-              />
-              Estou devidamente regularizado e sem nenhuma pendência nos órgãos
-              de trânsito (DETRAN e DENATRAN).
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                name="checkbox5"
-                checked={formData.checkboxes.checkbox5}
-                onChange={handleChange}
-              />
-              Atesto para os devidos fins legais que todas às informações
-              fornecidas são verdadeiras, se valendo e fazendo cumprir as
-              normativas da Lei Geral de Proteção de Dados (LGPD - Lei nº
-              13.709/2018), o Código Civil Brasileiro (Lei nº 10.406/2002) e o
-              previsto pelo Artigo 299 do Código Penal Brasileiro, que tipifica
-              como crime a falsidade ideológica.
-            </label>
-          </div>
-          <button id="register-button" type="submit">
-            Registrar
-          </button>
+      <div className="two-column-form">
+        <div className="form-group">
+          <label>Data de Nascimento</label>
+          <input
+            type="date"
+            name="birthday"
+            value={formData.birthday}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>CNH</label>
+          <input
+            type="text"
+            name="number_driver_license"
+            value={formData.number_driver_license}
+            onChange={handleChange}
+            required
+          />
         </div>
       </div>
+
+      <div className="two-column-form">
+        <div className="form-group">
+          <label>Rua</label>
+          <input
+            type="text"
+            name="street_name"
+            value={formData.street_name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Número</label>
+          <input
+            type="text"
+            name="number"
+            value={formData.number}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="two-column-form">
+        <div className="form-group">
+          <label>Bairro</label>
+          <input
+            type="text"
+            name="district"
+            value={formData.district}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Cidade</label>
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="two-column-form">
+        <div className="form-group">
+          <label>Celular</label>
+          <input
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <span>Será enviado um e-mail de confirmação</span>
+        </div>
+      </div>
+
+      <div className="two-column-form">
+  <div className="form-group">
+    <label>Senha</label>
+    <div className="password-input-container">
+      <input
+        type={showPassword ? "text" : "password"}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="password-register"
+      />
+      <img
+        src={showPassword ? hidePasswordIcon : showPasswordIcon}
+        alt="Ícone de visualização"
+        className="password-icon"
+        onClick={togglePasswordVisibility}
+      />
     </div>
+  </div>
+
+  <div className="form-group">
+    <label>Confirmar Senha</label>
+    <div className="password-input-container">
+      <input
+        type={showPassword ? "text" : "password"}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="password-register"
+      />
+      <img
+        src={showPassword ? hidePasswordIcon : showPasswordIcon}
+        alt="Ícone de visualização"
+        className="password-icon"
+        onClick={togglePasswordVisibility}
+      />
+    </div>
+  </div>
+</div>
+      
+      <span>
+        A senha deve conter pelo menos 12 caracteres, incluindo uma letra maiúscula, um número e um símbolo.
+      </span>
+
+      <div className="checkbox-group">
+        <label>
+          <input
+            type="checkbox"
+            name="checkbox1"
+            checked={formData.checkboxes.checkbox1}
+            onChange={handleChange}
+          />
+          Aceita receber comunicações via WhatsApp
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="checkbox2"
+            checked={formData.checkboxes.checkbox2}
+            onChange={handleChange}
+          />
+          Aceita os Termos de Uso
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="checkbox3"
+            checked={formData.checkboxes.checkbox3}
+            onChange={handleChange}
+          />
+          Aceita a Política de Privacidade
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="checkbox4"
+            checked={formData.checkboxes.checkbox4}
+            onChange={handleChange}
+          />
+          Estou devidamente regularizado e sem pendências nos órgãos de trânsito (DETRAN, DENATRAN).
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="checkbox5"
+            checked={formData.checkboxes.checkbox5}
+            onChange={handleChange}
+          />
+          Atesto que todas as informações fornecidas são verdadeiras e concordo com as leis vigentes.
+        </label>
+      </div>
+
+      <button id="register-button" type="submit">
+        Registrar
+      </button>
+    </form>
+  </Modal>
   );
 };
 
