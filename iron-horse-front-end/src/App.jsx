be7e3920b44c14f860f4b24b0838b3a00c970d24
Emmanoel <emmanoel_95@hotmail.com>
2024-11-12 17:29:31 -0300

    feat(App.jsx): add Router to App

---
+++ iron-horse-front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+import './assets/styles/global.css';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <>
+
+      <Router>
+      <Routes>
+
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
