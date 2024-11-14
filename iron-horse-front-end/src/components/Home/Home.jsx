c6f6c08665af90623dde1d9641270b6cee192631
evairlf <evairleitefeldmann@gmail.com>
2024-11-13 21:23:17 -0300

    refactor: added component Footer

--- iron-horse-front-end/src/components/Home/Home.jsx
+++ iron-horse-front-end/src/components/Home/Home.jsx
@@ -1,23 +1,65 @@
+import { useState } from "react";
 import NavigationBar from "../NavigationBar";
 import SearchBar from "../SearchBar/SearchBar";
+import Footer from "../Footer/Footer";
 import './Home.css';
 import Carousel from "../Carousel/Carousel";
+import Modal from "../Modal/Modal"; // Certifique-se de que o Modal está sendo importado corretamente
+import TermsOfUse from "../Footer/Options/TermsOfUse"; // Certifique-se de importar os componentes de cada modal
+import PrivacyPolicy from "../Footer/Options/PrivacyPolicy"; // Importando o componente de Política de Privacidade
+import LegalNotice from "../Footer/Options/LegalNotice";
+import Support from "../Footer/Options/Support";
 
+function Home() {
+    const [activePopup, setActivePopup] = useState(null);
 
-function Home () {
-    return(
+    const openPopup = (popupType) => {
+        setActivePopup(popupType);
+    };
+
+    const closePopup = () => {
+        setActivePopup(null);
+    };
+
+    return (
         <>
-        <div className="home-main-container">        
-        <NavigationBar/>
-        
-            <main>            
-                <SearchBar/>
-                <Carousel/>
-                <h2>Alugue praticamente qualquer carro, em qualquer lugar!</h2>                     
-            </main> 
+            <div className="home-main-container">
+                <NavigationBar />
+
+                <main>
+                    <SearchBar />
+                    <Carousel />
+                    <h2>Alugue praticamente qualquer carro, em qualquer lugar!</h2>
+                </main>
+
+                <Footer onOpenModal={openPopup} />
             </div>
+
+            {activePopup === "termos" && (
+                <Modal isOpen={true} onClose={closePopup}>
+                    <TermsOfUse />
+                </Modal>
+            )}
+
+            {activePopup === "privacidade" && (
+                <Modal isOpen={true} onClose={closePopup}>
+                    <PrivacyPolicy />
+                </Modal>
+            )}
+
+            {activePopup === "avisos" && (
+                <Modal isOpen={true} onClose={closePopup}>
+                    <LegalNotice />
+                </Modal>
+            )}
+
+            {activePopup === "suporte" && (
+                <Modal isOpen={true} onClose={closePopup}>
+                    <Support />
+                </Modal>
+            )}
         </>
-    )
+    );
 }
 
 export default Home;
