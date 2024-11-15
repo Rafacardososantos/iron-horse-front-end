110dc63ce1eaa21008f2f41e53244b0765b187a2
Evairlf <evairleitefeldmann@gmail.com>
2024-11-15 16:26:37 -0300

    refactor: added protected route with authorization context

--- iron-horse-front-end/src/main.jsx
+++ iron-horse-front-end/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './components/Home/Home'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AuthProvider } from './context/AuthContext';
+import { CarProvider } from './context/CarContext';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home/>
+    <AuthProvider>
+      <CarProvider>
+        <App />
+      </CarProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
