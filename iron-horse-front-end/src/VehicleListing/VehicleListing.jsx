6ede67cb70b539a6eabad9459a17c5ed8378a2f4
Emmanoel <emmanoel_95@hotmail.com>
2024-11-17 18:30:13 -0300

    refactor(VehicleListing): add validation with dates

--- iron-horse-front-end/src/VehicleListing/VehicleListing.jsx
+++ iron-horse-front-end/src/VehicleListing/VehicleListing.jsx
@@ -2,7 +2,7 @@ import './VehicleListing.css';
 import carImage1 from '../../public/img/Hatch background1.jpg';
 import SearchBar from '../components/SearchBar/SearchBar';
 import { useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import NavigationBar from '../components/NavigationBar';
 import api from '../utils/api';
 
@@ -13,13 +13,42 @@ const VehicleListing = () => {
   const totalPages = state?.totalPages || 0;
   const currentPage = state?.currentPage || 0;
   const city = state?.city || '';
+  const startDate = state?.startDate || '';
+  const endDate = state?.endDate || '';
 
   const [results, setResults] = useState(initialResults);
   const [page, setPage] = useState(currentPage);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(currentPage < totalPages - 1);
 
-  const fetchNextPage = async () => {
+  const reloadSearch = async () => {
+    setPage(0);
+    setResults([]);
+    setHasMore(true);
+    setLoading(true);
+
+    try {
+      const response = await api.get('/cars/search', {
+        params: {
+          city,
+          startDate,
+          endDate,
+          page: 0,
+          size: 10,
+        },
+      });
+
+      setResults(response.content);
+      setPage(1);
+      setHasMore(response.number < response.totalPages - 1);
+    } catch (error) {
+      console.error('Erro ao carregar carros:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchNextPage = useCallback(async () => {
     if (loading || !hasMore) return;
 
     setLoading(true);
@@ -28,70 +57,59 @@ const VehicleListing = () => {
       const response = await api.get('/cars/search', {
         params: {
           city,
+          startDate,
+          endDate,
           page: page + 1,
           size: 10,
         },
       });
 
-      setResults(prevResults => [...prevResults, ...response.content]);
-      setPage(prevPage => prevPage + 1);
-      setHasMore(response.number < response.totalPages - 1);
+      if (response.content.length > 0) {
+        setResults((prevResults) => [...prevResults, ...response.content]);
+        setPage((prevPage) => prevPage + 1);
+        setHasMore(response.number < response.totalPages - 1);
+      } else {
+        setHasMore(false);
+      }
     } catch (error) {
-      console.error("Erro ao carregar mais carros:", error);
+      console.error('Erro ao carregar mais carros:', error);
     } finally {
       setLoading(false);
     }
-  };
+  }, [loading, hasMore, city, startDate, endDate, page]);
 
   useEffect(() => {
-    const handleScroll = (event) => {
-      const bottom = event.target.scrollHeight === event.target.scrollTop + event.target.clientHeight;
-      if (bottom) {
-        fetchNextPage();
-      }
-    };
+    if (state?.results && state?.results.length > 0) {
+      setResults(state.results);
+      setPage(state.currentPage);
+      setHasMore(state.currentPage < state.totalPages - 1);
+    } else {
+      reloadSearch();
+    }
+  }, [state?.results, state?.city, state?.startDate, state?.endDate]);
 
-    const scrollContainer = document.getElementById("results-container");
-    scrollContainer.addEventListener("scroll", handleScroll);
+  const handleScroll = useCallback((event) => {
+    const scrollContainer = event.target;
+    const bottom =
+      scrollContainer.scrollHeight === scrollContainer.scrollTop + scrollContainer.clientHeight;
+
+    if (bottom && !loading && hasMore) {
+      fetchNextPage();
+    }
+  }, [loading, hasMore, fetchNextPage]);
+
+  useEffect(() => {
+    const scrollContainer = document.getElementById('results-container');
+    if (scrollContainer) {
+      scrollContainer.addEventListener('scroll', handleScroll);
+    }
 
     return () => {
-      scrollContainer.removeEventListener("scroll", handleScroll);
+      if (scrollContainer) {
+        scrollContainer.removeEventListener('scroll', handleScroll);
+      }
     };
-  }, [loading, hasMore]);
-
-  useEffect(() => {
-    const initMap = () => {
-      const defaultLocation = { lat: -30.0277, lng: -51.2287 };
-      const mapCenter = results.length > 0 ? 
-      { lat: results[0].latitude, lng: results[0].longitude } : 
-      defaultLocation;
-
-      const map = new window.google.maps.Map(document.getElementById("map"), {
-        zoom: 13,
-        center: mapCenter,
-      });
-
-      results.forEach(car => {
-        new window.google.maps.Marker({
-          position: { lat: car.latitude, lng: car.longitude },
-          map: map,
-          label: car.brand + " " + car.model + " " + car.manufactureYear,
-        });
-      });
-    };
-
-    if (!window.google) {
-      const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=APIKEY&callback=initMap`;
-      script.async = true;
-      script.defer = true;
-      document.body.appendChild(script);
-
-      window.initMap = initMap;
-    } else {
-      initMap();
-    }
-  }, [results]);
+  }, [handleScroll]); // O efeito é recriado apenas se 'handleScroll' mudar
 
   return (
     <div>
@@ -118,7 +136,7 @@ const VehicleListing = () => {
                   <img src={car.path || carImage1} alt={car.name || 'Carro'} />
                 </div>
                 <div className="car-details">
-                  <h3>{car.brand + " " + car.model + " " + car.manufactureYear || 'Nome do Carro'}</h3>
+                  <h3>{car.brand + ' ' + car.model + ' ' + car.manufactureYear || 'Nome do Carro'}</h3>
                   <p className="car-price">Valor da diária R$ {car.price.toFixed(2) || 'Preço'}</p>
                   <p className="car-location">{car.city || 'Localização'}</p>
                 </div>
