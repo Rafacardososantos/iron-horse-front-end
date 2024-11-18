b0ff34c1324da0ff8baec53a901cb9994c0a4c5d
Rafacardososantos <43182098+Rafacardososantos@users.noreply.github.com>
2024-11-18 20:59:30 -0300

    Adding rental list route

--- iron-horse-front-end/src/App.jsx
+++ iron-horse-front-end/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home'
+import RentaList from './RentalList/RentalList';
 
 function App() {
 
-  
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rentals" element={
+              <RentaList/>
+          }/>
         </Routes>
     </Router>
   );
