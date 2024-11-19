48a8adab1580c4592473b4f32c5104ac81c1f2ff
Evairlf <evairleitefeldmann@gmail.com>
2024-11-19 15:53:44 -0300

    refactor(LoginPopUp): added toast

--- iron-horse-front-end/src/LoginPopup/LoginPopup.jsx
+++ iron-horse-front-end/src/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./LoginPopup.css";
+import React from 'react';
 import showPasswordIcon from "../img/Visualização_Permitida.png";
 import hidePasswordIcon from "/img/Visualizar.png";
 import Modal from '../components/Modal/Modal';
 import api from "../utils/api";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const LoginPopup = ({ onClose, openForgotPassword, openSignUp }) => {
   const [email, setEmail] = useState("");
@@ -12,24 +16,28 @@ const LoginPopup = ({ onClose, openForgotPassword, openSignUp }) => {
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState('');
   const [isModalOpen, setModalOpen] = useState(true);
+  const [refresh, setRefresh] = React.useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await api.post("auth/login", {email, password});
+      const response = await api.post("auth/login", { email, password });
 
       if (response.accessToken) {
         localStorage.setItem("accessToken", response.accessToken);
         localStorage.setItem("refreshToken", response.refreshToken);
-        alert("Usuario logado com sucesso");
-        onClose(); 
-        window.location.reload();
+        toast.success('USUARIO LOGADO COM SUCESSO!');
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000);
+
       } else {
         console.error("Erro ao fazer login");
       }
     } catch (error) {
       console.error("Erro ao enviar dados de login", error);
       setError("Erro ao realizar login: " + error.response.data.title);
+      toast.error("Erro ao realizar login: " + error.response.data.title);
     }
   };
 
@@ -45,59 +53,60 @@ const LoginPopup = ({ onClose, openForgotPassword, openSignUp }) => {
 
   return (
     <>
-    <Modal isOpen={isModalOpen} onClose={onClose}>
-      <div className="login-page">
-        <h2>Login</h2>
-        {error && <p className="error-message">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <input className="login-input"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <div className="input-container">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="password-input"
+      <Modal isOpen={isModalOpen} onClose={onClose}>
+        <ToastContainer />
+        <div className="login-page">
+          <h2>Login</h2>
+          {error && <p className="error-message">{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <input className="login-input"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <img
-              src={showPassword ? hidePasswordIcon : showPasswordIcon}
-              alt="Ícone de visualização"
-              className="password-icon"
-              onClick={togglePasswordVisibility}
-            />
-          </div>
-          <button type="submit" className="login-btn">
-            Entrar
-          </button>
-        </form>
-        <p>
-          <a
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              openForgotPassword();
-            }}
-          >
-            Esqueceu a senha?
-          </a>
-          <br />
-          Não possui conta?{" "}
-          <b
-            onClick={(e) => {
-              e.preventDefault();
-              openSignUp();
-            }}
-          >
-            Cadastre-se
-          </b>
-        </p>
+            <div className="input-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="password-input"
+              />
+              <img
+                src={showPassword ? hidePasswordIcon : showPasswordIcon}
+                alt="Ícone de visualização"
+                className="password-icon"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+            <button type="submit" className="login-btn">
+              Entrar
+            </button>
+          </form>
+          <p>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                openForgotPassword();
+              }}
+            >
+              Esqueceu a senha?
+            </a>
+            <br />
+            Não possui conta?{" "}
+            <b
+              onClick={(e) => {
+                e.preventDefault();
+                openSignUp();
+              }}
+            >
+              Cadastre-se
+            </b>
+          </p>
         </div>
-    </Modal>
+      </Modal>
     </>
   );
 };
