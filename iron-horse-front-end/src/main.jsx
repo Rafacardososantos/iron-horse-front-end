799a8e7f8af281be852464ba2c841cddedcca61f
cristinasstemmer <cristinasstemmer@gmail.com>
2024-11-19 23:58:10 -0300

    refactor: exclusao arquivos inutilizados na branch

--- iron-horse-front-end/src/main.jsx
+++ iron-horse-front-end/src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import CreateAccount from './CreateAccount/CreateAccount'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CreateAccount/>
   </StrictMode>,
 )
