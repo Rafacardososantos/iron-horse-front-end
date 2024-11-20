3e16148be73bdde880ee770d15b94863f2a4c8c7
evairlf <evairleitefeldmann@gmail.com>
2024-11-19 22:27:49 -0300

    refactor(app): added reset password route

--- iron-horse-front-end/src/App.jsx
+++ iron-horse-front-end/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home'
 import VehicleListing from './VehicleListing/VehicleListing';
 import CarInfo from './CarInfo/CarInfo'
 import ProtectedRoute from './components/ProtectedRoute';
+import ResetPassword from './ResetPassword/resetPassword';
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/vehicle-listing" element={<VehicleListing />} />
           <Route path="/car-details" element={<ProtectedRoute><CarInfo /></ProtectedRoute>} />
+          <Route
+          path="/recovery/reset"
+          element={<ResetPassword />}
+        />
         </Routes>
     </Router>
   );
