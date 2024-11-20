c306b09a1fd5155e6e45e3969e32d6c201417c32
Rafacardososantos <43182098+Rafacardososantos@users.noreply.github.com>
2024-11-20 20:30:24 -0300

    testing

--- iron-horse-front-end/src/main.jsx
+++ iron-horse-front-end/src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './context/AuthContext';
 import { CarProvider } from './context/CarContext';
 import App from './App';
-import AllRentals from './AllRentals/AllRentals';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <CarProvider>
-        <AllRentals />
+        <App />
       </CarProvider>
     </AuthProvider>
   </StrictMode>
