681b111122604bbbc156312c69c29d0f3124995f
cristinasstemmer <cristinasstemmer@gmail.com>
2024-11-19 21:42:37 -0300

    style: responsividade e modularizacao

--- iron-horse-front-end/src/Confirmation/QrCodePage.jsx
+++ iron-horse-front-end/src/Confirmation/QrCodePage.jsx
@@ -1,22 +1,24 @@
 import NavigationBar from "../components/NavigationBar";
-import "./QrCodePage.css";
+import styles from "./QrCodePage.module.css";
 
 function Confirmation() {
     return (
-        <div className="confirmation-container">
+        <div className={styles.pageContainer}>
             <NavigationBar />
-            <h1 className="confirmation-title">Viagem Confirmada!</h1>
-            <div className="confirmation-content">
-                <div className="confirmation-icon">
-                    <img src="../img/Check_img.png" alt="Ícone de Confirmação" />
+            <div className={styles.confirmationContainer}>
+                <div className={styles.confirmationContent}>
+                    <h1 className={styles.confirmationTitle}>Viagem Confirmada!</h1>
+                    <div className={styles.confirmationIcon}>
+                        <img src="../img/Check_img.png" alt="Ícone de Confirmação" />
+                    </div>
+                    <p className={styles.confirmationDate}>09 de Outubro de 2024</p>
+                    <p className={styles.confirmationTime}>20:35</p>
+                    <button className={styles.confirmationButton}>Avançar</button>
                 </div>
-                <p className="confirmation-date">09 de Outubro de 2024</p>
-                <p className="confirmation-time">20:35</p>
-                <button className="confirmation-button">Avançar</button>
-            </div>
-            <div className="verification-container">
-                <h2 className="verification-title">Verificação</h2>
-                <img src="../img/QR_code_img.png" alt="QR Code" className="qrcode-image" />
+                <div className={styles.verificationContainer}>
+                    <h2 className={styles.verificationTitle}>Verificação</h2>
+                    <img src="../img/QR_code_img.png" alt="QR Code" className={styles.qrcodeImage} />
+                </div>
             </div>
         </div>
     );
