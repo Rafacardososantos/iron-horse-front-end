37d0629570c614d2d78139cf6efa623095b24a9a
cristinasstemmer <cristinasstemmer@gmail.com>
2024-11-19 21:50:48 -0300

    style: modularizacao css

--- src/ImageRegister/ImageRegister.jsx
+++ src/ImageRegister/ImageRegister.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import "./ImageRegister.css";
+import styles from "./ImageRegister.module.css"; 
 
 const VehicleRegistrationModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -58,10 +58,9 @@ const VehicleRegistrationModal = () => {
     <div>
       <button onClick={openModal}>Abrir Popup</button>
 
-      {/* Modal */}
       {isModalOpen && (
-        <div className="modal-overlay">
-          <div className="modal-content">
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalContent}>
             <h2>Cadastre seu Veículo</h2>
 
             <input
@@ -71,68 +70,68 @@ const VehicleRegistrationModal = () => {
               style={{ display: 'none' }}
             />
 
-            <img src="../img/carro-ilustracao-de-transporte.png" alt="Logo" className="modal-image" />
+            <img src="../img/carro-ilustracao-de-transporte.png" alt="Logo" className={styles.modalImage} />
         
             <label>Imagens do Veículo</label>
             <span>Imagem de Capa ou Destaque do Anúncio</span>
-            <div className="image-container" onClick={handleImageClick}>
-              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className="upload-icon" />
+            <div className={styles.imageContainer} onClick={handleImageClick}>
+              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className={styles.uploadIcon} />
             </div>
 
             <span>Imagens que compõem o Anúncio e Protocolares para o Seguro:</span>
             <span>Imagem Frontal</span>
-            <img src="../img/vista frontal.png" alt="Vista Frontal" className="car-image" />
-            <div className="image-container" onClick={handleImageClick}>
-              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className="upload-icon" />
+            <img src="../img/vista frontal.png" alt="Vista Frontal" className={styles.carImage} />
+            <div className={styles.imageContainer} onClick={handleImageClick}>
+              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className={styles.uploadIcon} />
             </div>
 
             <span>Imagem Traseira</span>
-            <img src="../img/vista traseira.png" alt="Vista Traseira" className="car-image" />
-            <div className="image-container" onClick={handleImageClick}>
-              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className="upload-icon" />
+            <img src="../img/vista traseira.png" alt="Vista Traseira" className={styles.carImage} />
+            <div className={styles.imageContainer} onClick={handleImageClick}>
+              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className={styles.uploadIcon} />
             </div>
 
             <span>Imagem Lateral Direita</span>
-            <img src="../img/vista lateral direita.png" alt="Lateral Direita" className="lateral-car-image" />
-            <div className="image-container" onClick={handleImageClick}>
-              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className="upload-icon" />
+            <img src="../img/vista lateral direita.png" alt="Lateral Direita" className={styles.lateralCarImage} />
+            <div className={styles.imageContainer} onClick={handleImageClick}>
+              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className={styles.uploadIcon} />
             </div>
 
             <span>Imagem Lateral Esquerda</span>
-            <img src="../img/vista lateral esquerda.png" alt="Lateral Esquerda" className="lateral-car-image" />
-            <div className="image-container" onClick={handleImageClick}>
-              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className="upload-icon" />
+            <img src="../img/vista lateral esquerda.png" alt="Lateral Esquerda" className={styles.lateralCarImage} />
+            <div className={styles.imageContainer} onClick={handleImageClick}>
+              <img src="../img/Nuvem_Upload_Transparente_Seta.png" alt="Upload" className={styles.uploadIcon} />
             </div>
             
             <label>Consentimento</label>
-            <div className="checkbox-group">
+            <div className={styles.checkboxGroup}>
               <label>
                 <input type="checkbox" name="checkbox1" checked={vehicleData.consentCheckboxes.checkbox1} onChange={handleInputChange} />
-                <p className="checkbox-text">A documentação do carro está em dia...</p>
+                <p className={styles.checkboxText}>A documentação do carro está em dia...</p>
               </label>
               <label>
                 <input type="checkbox" name="checkbox2" checked={vehicleData.consentCheckboxes.checkbox2} onChange={handleInputChange} />
-                <p className="checkbox-text">Os condutores NÃO podem ser fumantes...</p>
+                <p className={styles.checkboxText}>Os condutores NÃO podem ser fumantes...</p>
               </label>
               <label>
                 <input type="checkbox" name="checkbox3" checked={vehicleData.consentCheckboxes.checkbox3} onChange={handleInputChange} />
-                <p className="checkbox-text">Os valores de pedágio...</p>
+                <p className={styles.checkboxText}>Os valores de pedágio...</p>
               </label>
               <label>
                 <input type="checkbox" name="checkbox4" checked={vehicleData.consentCheckboxes.checkbox4} onChange={handleInputChange} />
-                <p className="checkbox-text">As multas contraídas...</p>
+                <p className={styles.checkboxText}>As multas contraídas...</p>
               </label>
               <label>
                 <input type="checkbox" name="checkbox5" checked={vehicleData.consentCheckboxes.checkbox5} onChange={handleInputChange} />
-                <p className="checkbox-text">O veículo caso possua modificações...</p>
+                <p className={styles.checkboxText}>O veículo caso possua modificações...</p>
               </label>
               <label>
                 <input type="checkbox" name="checkbox6" checked={vehicleData.consentCheckboxes.checkbox6} onChange={handleInputChange} />
-                <p className="checkbox-text">Atesto para os devidos fins legais...</p>
+                <p className={styles.checkboxText}>Atesto para os devidos fins legais...</p>
               </label>
             </div>
 
-            <div className="button-container">
+            <div className={styles.buttonContainer}>
               <button id="cancel-button" type="button" onClick={closeModal}>Cancelar</button>
               <button id="register-button" type="submit" onClick={handleSubmit}>Prosseguir</button>
             </div>
