2038955f9d9d1f6e28ae1176273ceb56d829bf2c
Rafacardososantos <43182098+Rafacardososantos@users.noreply.github.com>
2024-11-20 22:20:38 -0300

    Adjusting checkbox

--- iron-horse-front-end/src/CarOverview/CarOverview.jsx
+++ iron-horse-front-end/src/CarOverview/CarOverview.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import styles from "./CarOverview.module.css";
 import Modal from "../components/Modal/Modal";
 import api from "../utils/api";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CarOverview = ({ onClose, car }) => {
   const [error, setError] = useState("");
@@ -17,143 +17,146 @@ const CarOverview = ({ onClose, car }) => {
   useEffect(() => {
     const fetchCarDetails = async () => {
       try {
-        const accessToken = localStorage.getItem('accessToken');
+        const accessToken = localStorage.getItem("accessToken");
         if (!accessToken) {
-          throw new Error('Token de autenticação não encontrado');
+          throw new Error("Token de autenticação não encontrado");
         }
-  
-        const response = await fetch(`http://localhost:8080/v1/car-overviews/${car.id}`, {
-          method: 'GET', 
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`, 
-          },
-        });
-  
+
+        const response = await fetch(
+          `http://localhost:8080/v1/car-overviews/${car.id}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
         if (!response.ok) {
-          throw new Error('Erro ao buscar os detalhes do carro');
+          throw new Error("Erro ao buscar os detalhes do carro");
         }
-  
+
         const carData = await response.json();
-        console.log(carData);
         setDescription(carData.description || "sem nada");
         setCarValue(carData.price || "");
         setIsActive(carData.isActive);
         setIsAvailable(true);
-  
+
         console.log(carData.description, carData.price, carData.isActive, true);
       } catch (error) {
         console.error("Erro ao buscar os detalhes do carro", error);
         setError("Erro ao carregar os detalhes do carro.");
       }
     };
-  
+
     if (car.id) {
       fetchCarDetails();
     }
   }, [car]);
-  
 
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const price = carValue ? parseFloat(carValue) : null;
-  
-      const accessToken = localStorage.getItem('accessToken');
-      if (!accessToken) {
-        throw new Error('Token de autenticação não encontrado');
-      }
-  
-      const response = await fetch(`http://localhost:8080/v1/car-overviews/${car.id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          description,
-          isActive,
-          isAvailable,
-          price,
-        }),
-      });
 
-      if(response.ok){
-        toast.success('Dados modificados com sucesso!');
+      const accessToken = localStorage.getItem("accessToken");
+      if (!accessToken) {
+        throw new Error("Token de autenticação não encontrado");
+      }
+
+      const response = await fetch(
+        `http://localhost:8080/v1/car-overviews/${car.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify({
+            description,
+            isActive,
+            isAvailable,
+            price,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        toast.success("Dados modificados com sucesso!");
         setTimeout(() => {
-          window.location.reload(); 
+          window.location.reload();
         }, 2000);
       }
-  
+
       if (!response.ok) {
         throw new Error(`Erro HTTP: ${response.status}`);
       }
-
     } catch (error) {
       console.error("Erro ao enviar dados do carro", error);
       setError("Erro ao salvar informações do carro: " + error.message);
     }
   };
 
-const formatCurrency = (value) => {
-  if (!value) return "";
-  const number = parseFloat(value) / 100;
-  return number.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-};
+  const formatCurrency = (value) => {
+    if (!value) return "";
+    const number = parseFloat(value) / 100;
+    return number.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
 
-const handleChange = (e) => {
-  const rawValue = e.target.value.replace(/\D/g, ""); 
-  setCarValue(rawValue); 
-};
-  
+  const handleChange = (e) => {
+    const rawValue = e.target.value.replace(/\D/g, "");
+    setCarValue(rawValue);
+  };
+
   return (
     <Modal isOpen={isModalOpen} onClose={onClose}>
-    <ToastContainer />
-    <div className={styles.CarOverview}>
-      <h2>Informações do Carro</h2>
-      {error && <p className={styles.errorMessage}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Ativo
-            <input
-              type="checkbox"
-              checked={isActive}
-              onChange={(e) => setIsActive(e.target.checked)}
-              className={styles.buttonActive}
-            />
-          </label>
-        </div>
+      <ToastContainer />
+      <div className={styles.CarOverview}>
+        <h2>Informações do Carro</h2>
+        {error && <p className={styles.errorMessage}>{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className={styles.checkboxInput}>
+            <label className={styles.checkboxLabel}>Ativo</label>
+            <label className={styles.toggleButton}>
+              <input
+                type="checkbox"
+                checked={isActive}
+                onChange={(e) => setIsActive(e.target.checked)}
+                className={styles.buttonActive}
+              />
+              <span className={styles.slider}></span>
+            </label>
+          </div>
 
-        <input
-          type="number"
-          placeholder="Valor da diária do carro"
-          value={carValue}
-          onChange={handleChange}
-          className={styles.carInput}
-        />
-        <textarea
-          placeholder="Descrição"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          maxLength={255}
-          className={styles.descriptionInput}
-        />
-        <p>{description.length}/255 caracteres</p>
+          <input
+            type="number"
+            placeholder="Valor da diária do carro"
+            value={carValue}
+            onChange={handleChange}
+            className={styles.carInput}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            maxLength={255}
+            className={styles.descriptionInput}
+          />
+          <p>{description.length}/255 caracteres</p>
 
-        <button type="submit" className={styles.saveBtn}>
-          Salvar
-        </button>
-      </form>
-    </div>
-  </Modal>
+          <button type="submit" className={styles.saveBtn}>
+            Salvar
+          </button>
+        </form>
+      </div>
+    </Modal>
   );
 };
 
