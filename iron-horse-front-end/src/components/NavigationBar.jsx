737b7b0c47f487299b4d25fc5ad0d3847a820d56
Rafacardososantos <43182098+Rafacardososantos@users.noreply.github.com>
2024-11-21 11:30:09 -0300

    Removing unused code

--- iron-horse-front-end/src/components/NavigationBar.jsx
+++ iron-horse-front-end/src/components/NavigationBar.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LoginPopup from "../LoginPopup/LoginPopup";
 import CreateAccount from "../Create-Account/CreateAccount";
 import "./NavigationBar.css";
-import { useNavigate } from 'react-router-dom';
-import CarRegister from "../CarRegister/CarRegister"
+import CarRegister from "../CarRegister/CarRegister";
 import ForgotPassword from "../ForgotPassword/ForgotPassword";
 
-export default () => {
+function NavigationBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activePopup, setActivePopup] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -19,9 +19,9 @@ export default () => {
   }, []);
 
   function logout() {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    window.location.href = '/'; 
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    window.location.href = "/";
   }
 
   const goToMyCars = () => {
@@ -47,111 +47,115 @@ export default () => {
 
   return (
     <>
-       <header>
-      <div className="logo-img">
-        <img
-          className="user-profile-img"
-          src="/img/Logo_UVIO_contrario.png"
-          alt="Logo UVIO"
-          onClick={home}
-        />
-      </div>
-
-      <div className="user-img" onClick={toggleMenu}>
-        <img
-          className="user-profile-img"
-          src="/img/Perfil-Usuario.png"
-          alt="Imagem do usuário"
-        />
-      </div>
-      {isMenuOpen && (
-        <div className="dropdown-menu">
-          {isLoggedIn ? (
-            // Exibe "Meus Carros", "Anunciar", e "Logout" somente se o usuário estiver logado
-            <>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    goToMyCars(); // Chama a página "Meus Carros"
-                    toggleMenu();
-                  }}
-                >
-                  Meus Carros
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    openPopup("CarRegister");
-                    toggleMenu();
-                  }}
-                >
-                  Anunciar
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    logout();
-                    toggleMenu();
-                  }}
-                >
-                  Logout
-                </a>
-              </li>
-            </>
-          ) : (
-            // Exibe "Sign In" e "Sign Up" somente se o usuário não estiver logado
-            <>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    openPopup("login");
-                    toggleMenu();
-                  }}
-                >
-                  Sign In
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    openPopup("signUp");
-                    toggleMenu();
-                  }}
-                >
-                  Sign Up
-                </a>
-              </li>
-            </>
-          )}
+      <header>
+        <div className="logo-img">
+          <img
+            className="user-profile-img"
+            src="/img/Logo_UVIO_contrario.png"
+            alt="Logo UVIO"
+            onClick={home}
+          />
         </div>
+
+        <div className="user-img" onClick={toggleMenu}>
+          <img
+            className="user-profile-img"
+            src="/img/Perfil-Usuario.png"
+            alt="Imagem do usuário"
+          />
+        </div>
+        {isMenuOpen && (
+          <div className="dropdown-menu">
+            <ul>
+              {isLoggedIn ? (
+                <>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        goToMyCars();
+                        toggleMenu();
+                      }}
+                    >
+                      Meus Carros
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        openPopup("CarRegister");
+                        toggleMenu();
+                      }}
+                    >
+                      Anunciar
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        logout();
+                        toggleMenu();
+                      }}
+                    >
+                      Logout
+                    </a>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        openPopup("login");
+                        toggleMenu();
+                      }}
+                    >
+                      Sign In
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        openPopup("signUp");
+                        toggleMenu();
+                      }}
+                    >
+                      Sign Up
+                    </a>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        )}
+      </header>
+
+      {activePopup === "login" && (
+        <LoginPopup
+          onClose={closePopup}
+          openSignUp={() => openPopup("signUp")}
+          openForgotPassword={() => openPopup("forgotPassword")}
+        />
       )}
-    </header>
 
-    {activePopup === "login" && (
-      <LoginPopup
-        onClose={closePopup}
-        openSignUp={() => openPopup("signUp")}
-        openForgotPassword={() => openPopup("forgotPassword")}
-      />
-    )}
+      {activePopup === "signUp" && <CreateAccount onClose={closePopup} />}
 
-    {activePopup === "signUp" && < CreateAccount onClose={closePopup} />}
+      {activePopup === "forgotPassword" && (
+        <ForgotPassword onClose={closePopup} />
+      )}
 
-    {activePopup === "forgotPassword" && <ForgotPassword onClose={closePopup} />}
-
-    {activePopup === "CarRegister" && <CarRegister onClose={closePopup} />}
-  </>
+      {activePopup === "CarRegister" && <CarRegister onClose={closePopup} />}
+    </>
   );
-};
+}
+
+export default NavigationBar;
